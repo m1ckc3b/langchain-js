d8dc927b17ce245743f52db44df5abf604acdaaa
mickceb <cebulski.mi@gmail.com>
2024-04-18 15:41:49 +0200

    implement rag with multi query

--- index.ts
+++ index.ts
@@ -7,61 +7,89 @@ import { StringOutputParser } from "langchain/schema/output_parser";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
 
-/** INDEXING 
+/** INDEXING
  * Index makes documents easy to retrieve
-*/
+ */
 
 // Load Documents
-const loader = new CheerioWebBaseLoader("url",{})
-const docs = await loader.load()
+const loader = new CheerioWebBaseLoader(
+  "https://lilianweng.github.io/posts/2023-06-23-agent/",
+  {
+    selector: "div.post-content",
+  }
+);
+const docs = await loader.load();
 
-// Split 
+// Split
 const splitter = new RecursiveCharacterTextSplitter({
   chunkSize: 1000,
-  chunkOverlap: 200
-})
-const splits = await splitter.splitDocuments(docs)
+  chunkOverlap: 200,
+});
+const splits = await splitter.splitDocuments(docs);
 
 // Embed
 const vectorStore = await MemoryVectorStore.fromDocuments(
   docs,
   new OpenAIEmbeddings()
-)
-const retriever = vectorStore.asRetriever({})
-const documents = await retriever.getRelevantDocuments("")
+);
+const retriever = vectorStore.asRetriever({});
 
-/** RETRIEVAL and GENERATION 
+/** RETRIEVAL and GENERATION
  * Retrieval powered via similarity search
-*/
-
-// Prompt
-const prompt = PromptTemplate.fromTemplate(`
-    Answer the question based only on the following context: {context}
-    
-    Question: {question}
-`)
+ */
 
 // LLM
 const llm = new ChatOpenAI({
-  modelName: "gpt-3.5-turbo",
-  temperature: 0
-})
+  modelName: "gpt-3.5-turbo-16k",
+  temperature: 0,
+});
 
-// Post-processing
-function formatDocs(docs) {
-  return docs.map(doc => doc.pageContent).join("\n\n")
+// Prompt -> Multi Query : Different perspectives
+const promptPerspectives = PromptTemplate.fromTemplate(`
+  You are an AI language model assistant. Your task is to generate five 
+  different versions of the given user question to retrieve relevant documents from a vector 
+  database. By generating multiple perspectives on the user question, your goal is to help
+  the user overcome some of the limitations of the distance-based similarity search. 
+  Provide these alternative questions separated by newlines. Original question: {question}
+`);
+
+const prompt = PromptTemplate.fromTemplate(`
+  Answer the following question based on this context: {context}
+  Question: {question}
+`);
+
+function getUniqueUnion(documents: Document[][]): Document[] {
+  // Flatten list of lists and convert each Document to string
+  const flattenedDocs: string[] = documents.flatMap((sublist) =>
+    sublist.map((doc) => JSON.stringify(doc))
+  );
+
+  // Get unique documents using Set for efficient deduplication
+  const uniqueDocsSet = new Set(flattenedDocs);
+
+  // Return the unique documents by parsing back from JSON strings
+  return Array.from(uniqueDocsSet).map((docString) => JSON.parse(docString));
 }
 
-// Chain
-const ragChain = RunnableSequence.from([
+const chainPerspectives = promptPerspectives
+  .pipe(llm)
+  .pipe(new StringOutputParser())
+  .pipe((x) => x.split("\n"));
+const retrievalChain = chainPerspectives
+  .pipe(retriever.map())
+  .pipe(getUniqueUnion);
+
+const finalChain = RunnableSequence.from([
   {
-    context: retriever.pipe(formatDocs),
-    question: new RunnablePassthrough()
+    context: retrievalChain,
+    question: new RunnablePassthrough(),
   },
   prompt,
   llm,
-  new StringOutputParser()
-])
+  new StringOutputParser(),
+]);
 
-// Question
-ragChain.invoke("my question is...")
+const response = await finalChain.invoke({
+  question: "What is task decomposition for LLM agents?"
+})
+console.log(response);
