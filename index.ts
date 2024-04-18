60c1172ecb25516d799e1aa1b22416cac23425b1
mickceb <cebulski.mi@gmail.com>
2024-04-18 14:15:10 +0200

    init basic RAG architecture

--- index.ts
+++ index.ts
@@ -1,14 +1,67 @@
-import { ChatOpenAI } from "@langchain/openai";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PromptTemplate } from "langchain/prompts";
+import { RunnablePassthrough, RunnableSequence } from "langchain/runnables";
+import { StringOutputParser } from "langchain/schema/output_parser";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
 
+/** INDEXING 
+ * Index makes documents easy to retrieve
+*/
 
-// Créer le model => ChatOpenAI
-const model = new ChatOpenAI({
-  openAIApiKey: process.env.OPENAI_API_KEY,
+// Load Documents
+const loader = new CheerioWebBaseLoader("url",{})
+const docs = await loader.load()
+
+// Split 
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 1000,
+  chunkOverlap: 200
+})
+const splits = await splitter.splitDocuments(docs)
+
+// Embed
+const vectorStore = await MemoryVectorStore.fromDocuments(
+  docs,
+  new OpenAIEmbeddings()
+)
+const retriever = vectorStore.asRetriever({})
+const documents = await retriever.getRelevantDocuments("")
+
+/** RETRIEVAL and GENERATION 
+ * Retrieval powered via similarity search
+*/
+
+// Prompt
+const prompt = PromptTemplate.fromTemplate(`
+    Answer the question based only on the following context: {context}
+    
+    Question: {question}
+`)
+
+// LLM
+const llm = new ChatOpenAI({
   modelName: "gpt-3.5-turbo",
-  verbose: true
+  temperature: 0
 })
 
-// Appeller le model => Promise<AIMessage>
-const response = await model.invoke("Raconte moi une histoire drole")
+// Post-processing
+function formatDocs(docs) {
+  return docs.map(doc => doc.pageContent).join("\n\n")
+}
 
-console.log(response);
+// Chain
+const ragChain = RunnableSequence.from([
+  {
+    context: retriever.pipe(formatDocs),
+    question: new RunnablePassthrough()
+  },
+  prompt,
+  llm,
+  new StringOutputParser()
+])
+
+// Question
+ragChain.invoke("my question is...")
