ea432bdbfa64b1a0c49e44aec6ff99977302c148
mickceb <cebulski.mi@gmail.com>
2024-04-19 14:03:26 +0200

    implements MultiQueryRetriever

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { PromptTemplate } from "langchain/prompts";
+import { ChatPromptTemplate, PromptTemplate, StringPromptValue } from "langchain/prompts";
+import { MultiQueryRetriever } from "langchain/retrievers/multi_query";
 import { RunnablePassthrough, RunnableSequence } from "langchain/runnables";
 import { StringOutputParser } from "langchain/schema/output_parser";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
@@ -29,10 +30,10 @@ const splits = await splitter.splitDocuments(docs);
 
 // Embed
 const vectorStore = await MemoryVectorStore.fromDocuments(
-  docs,
+  splits,
   new OpenAIEmbeddings()
 );
-const retriever = vectorStore.asRetriever({});
+
 
 /** RETRIEVAL and GENERATION
  * Retrieval powered via similarity search
@@ -41,55 +42,48 @@ const retriever = vectorStore.asRetriever({});
 // LLM
 const llm = new ChatOpenAI({
   modelName: "gpt-3.5-turbo-16k",
-  temperature: 0,
+  temperature: 0
 });
 
-// Prompt -> Multi Query : Different perspectives
-const promptPerspectives = PromptTemplate.fromTemplate(`
-  You are an AI language model assistant. Your task is to generate five 
-  different versions of the given user question to retrieve relevant documents from a vector 
-  database. By generating multiple perspectives on the user question, your goal is to help
-  the user overcome some of the limitations of the distance-based similarity search. 
-  Provide these alternative questions separated by newlines. Original question: {question}
-`);
+// Retriever
+const retriever = MultiQueryRetriever.fromLLM({
+  llm,
+  retriever: vectorStore.asRetriever()
+})
 
+
+
+// Prompt -> Multi Query : Different perspectives
+const promptRagFusion = PromptTemplate.fromTemplate(`
+You are a helpful assistant that generates multiple search queries based on a single input query.
+Generate multiple search queries related to: {question}
+Output (4 queries):
+`)
+
+
+function composeDocs(docs) {
+  return docs.map(doc => doc.pageContent).join("\n\n")
+}
+
+const retrievedDocs = promptRagFusion.pipe(new ChatOpenAI({
+  modelName: "gpt-3.5-turbo-16k",
+  temperature: 0
+})).pipe(new StringOutputParser()).pipe(retriever).pipe(composeDocs)
+
+// RAG
 const prompt = PromptTemplate.fromTemplate(`
   Answer the following question based on this context: {context}
   Question: {question}
-`);
+`)
 
-function getUniqueUnion(documents: Document[][]): Document[] {
-  // Flatten list of lists and convert each Document to string
-  const flattenedDocs: string[] = documents.flatMap((sublist) =>
-    sublist.map((doc) => JSON.stringify(doc))
-  );
-
-  // Get unique documents using Set for efficient deduplication
-  const uniqueDocsSet = new Set(flattenedDocs);
-
-  // Return the unique documents by parsing back from JSON strings
-  return Array.from(uniqueDocsSet).map((docString) => JSON.parse(docString));
-}
-
-const chainPerspectives = promptPerspectives
-  .pipe(llm)
-  .pipe(new StringOutputParser())
-  .pipe((x) => x.split("\n"));
-const retrievalChain = chainPerspectives
-  .pipe(retriever.map())
-  .pipe(getUniqueUnion);
-
-const finalChain = RunnableSequence.from([
+const chain = RunnableSequence.from([
   {
-    context: retrievalChain,
-    question: new RunnablePassthrough(),
+    context: retrievedDocs,
+    question: (input) => input.question
   },
   prompt,
   llm,
-  new StringOutputParser(),
-]);
+  new StringOutputParser()
+])
 
-const response = await finalChain.invoke({
-  question: "What is task decomposition for LLM agents?"
-})
-console.log(response);
+const result = chain.invoke({ question: "What is task decomposition for LLM agents?" })
